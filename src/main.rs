efad0f8958484bf1df6f2ae60b4288a7ac7b4927
gabrielsh06 <shgabriel06@gmail.com>
2026-09-05 01:30:30 -0500

    feat(main): add request and response structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+// structure for the request
+
+#[derive(Serialize)]
+struct Message {
+    role: String,
+    content: String,
+}
+
+#[derive(Serialize)]
+struct ChatRequest {
+    model: String,
+    messages: Vec<Message>,
+    temperature: f32,
+}
+
+// structure for the response
+
+#[derive(Deserialize)]
+struct ChatResponse {
+    choices: Vec<ChatChoide>,
+}
+
+#[derive(Deserialize)]
+struct ChatChoide {
+    message: MessageResponse,
+}
+
+#[derive(Deserialize)]
+struct MessageResponse {
+    content: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
